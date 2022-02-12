07defdecd108f66c34c2277d783578763a0a28bb
sanjoy <sanjoykundu187@gamil.com>
2022-02-11 23:38:56 -0500

    second math added

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 /* ১. 
 আমি যদি বলি কোন একটা এলিমেন্ট কে সিলেক্ট করে নিয়ে এসে console log করে দেখাতে হবে। তুমি কি সেটা করে দেখাতে পারবে?
 */
-var element = document.getElementById("change-element");
+var element = document.getElementById("card-title-first");
 console.log(element);
 
 
@@ -10,3 +10,12 @@ console.log(element);
 /* ২. 
 সেখানে একদম হেডার এ একটা buy now বাটন আছে। সেই বাটনে একটা ক্লিক হ্যান্ডলার যোগ করতে হবে? সেটা কি করতে পারবে? যদি পারো তাহলে সেই বাটনে ক্লিক করলে উপরে যেখানে BE THE PENGUIN এই কথাগুলো আছে সেখানে BE THE PENGUIN এর পরিবর্তে "I AM PENGUIN" কথাটা লিখে দিতে পারবে? 
  */
+
+
+document.getElementById("first-button").addEventListener("click", function () {
+	/* at first we can declear a variable and get the id and added .innerHTML to change the element value and then equal = set the new value */
+
+	const element = document.getElementById("change-element").innerHTML = "I AM PENGUIN";
+
+})
+
